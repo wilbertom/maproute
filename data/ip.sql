71e420176786edd7840130965c474509e8d7ae36
Wilberto Morales <wilbertomorales777@gmail.com>
2015-11-21 05:48:44 -0600

    Made ip_from and ip_to columns into integers

--- data/ip.sql
+++ data/ip.sql
@@ -1,8 +1,8 @@
 
 CREATE TABLE IF NOT EXISTS ips (
     -- id INTEGER,
-    ip_from TEXT UNIQUE,
-    ip_to TEXT UNIQUE,
+    ip_from INTEGER UNIQUE,
+    ip_to INTEGER UNIQUE,
     country_code TEXT,
     country_name TEXT,
     region_name TEXT,
